5dbdb159dc0760d263781df1f20878e81bf1668f
jonnyk1978 <eugene.krasyuk@gmail.com>
2020-12-02 15:39:32 +0700

    Добавил Radiobutton

---
+++ lesson08.py
@@ -0,0 +1,9 @@
+# Знакомство с виджетами. Виджет Radiobutton
+
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry(f"300x400+100+200")
+win.title('Мое первое графическое приложение')
+
+win.mainloop()
